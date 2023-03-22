c0d43f76c9def8a8e1cd8b24b42cf5b1b02dee93
b_jeptum <brendajeptum@gmail.com>
2023-03-22 09:22:51 -0700

    States and Cities Tables Updated

--- models/state.py
+++ models/state.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
+
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), default="", nullable=False)
+    cities = relationship("City", back_populates="state")
