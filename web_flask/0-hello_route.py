d0b2e04e2311f337dc855937b88a1e8697bcc637
JI-Maina <irungujmaina@gmail.com>
2023-04-23 12:42:44 +0000

    Pycodestyle requirements

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,10 +8,12 @@ use the option strict_slashes=False in your route definition
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
